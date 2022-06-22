ba30c37c0e2c3f0cdd1da345bf5b3194745309b8
shahid.dev <shahid.b2gsoft@gmail.com>
2022-06-22 18:25:26 +0600

    Update

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,6 @@
 const hostname = `https://onlinegame.demoff.xyz`;
 // const hostname = `http://192.168.5.248:`;
+// const hostname = `http://192.168.5.244:`;
 // const hostname = process.env.NODE_ENV === 'development' ? `http://192.168.5.248:` : `https://`;
 
 export default hostname;
