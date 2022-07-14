c513b70909d88a1b288466598be6f54fb9e96ae2
shahid.dev <shahid.b2gsoft@gmail.com>
2022-07-14 11:51:30 +0600

    updated

--- components/Layout/Footer/BottomFooter.tsx
+++ components/Layout/Footer/BottomFooter.tsx
@@ -3,7 +3,7 @@ export default function BottomFooter() {
   return (
     <div className={styles.main}>
       <p>
-        Repeat.gg is not endorsed by, directly affiliated with, maintained or
+        Ultimate Players Arena is not endorsed by, directly affiliated with, maintained or
         sponsored by Apple Inc, Electronic Arts, Activision Blizzard, Take-Two
         Interactive, Riot Games, Microsoft, Xbox, Sony, Playstation or Epic
         Games. All content, games titles, trade names and/or trade dress,
