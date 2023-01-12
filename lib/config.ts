0eacd67f7be6b34d9b7eafa954dfc7758094fadd
akibulshohag <akibulshohag@gmail.com>
2023-01-12 12:41:29 +0600

    up

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-const hostname = `http://onlinegame.demoff.xyz`;
+const hostname = `https://onlinegame.demoff.xyz`;
 // const hostname = `http://192.168.68.117/onlinegame/public`;
 // const hostname = `http://192.168.5.244:`;
 // const hostname = process.env.NODE_ENV === 'development' ? `http://192.168.5.248:` : `https://`;
