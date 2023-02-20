2d646180adf05df659353c86f1c45a5e1eb526a0
akibulshohag <akibulshohag@gmail.com>
2023-02-20 12:00:42 +0600

    up

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1295,7 +1295,7 @@ export default function Profile() {
                     />
                   ) : (
                     <Image
-                      src={"/assets/images/profile.png"}
+                      src={profileImage}
                       height={200}
                       width={200}
                       alt="profile"
